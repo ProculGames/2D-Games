7207f91433b1f7305e16be1dacd6aa7923996b2d
knudsjef <jeffery.knudson@gmail.com>
2015-12-23 14:17:43 -0600

    Added Movement script and basic scene
    I added a basic scene and movement script

---
+++ Assets/Scripts/Jeff's Scripts/PlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+    public GameObject Player;
+    public Rigidbody2D PlayerRigid;
+    public float Movespeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            PlayerRigid.velocity = new Vector2(PlayerRigid.velocity.x, Movespeed);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            PlayerRigid.velocity = new Vector2(PlayerRigid.velocity.x, -Movespeed);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            PlayerRigid.velocity = new Vector2(-Movespeed, PlayerRigid.velocity.y);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            PlayerRigid.velocity = new Vector2(Movespeed, PlayerRigid.velocity.y);
+        }
+	}
+}
