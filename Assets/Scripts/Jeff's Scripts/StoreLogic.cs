cdeba0bd15f39d6f986aaebb2bf64e8064e270bb
knudsjef <jeffery.knudson@gmail.com>
2015-12-24 07:07:49 -0600

    Added store to scene
    Player can now buy and place turret objects around the scene.

---
+++ Assets/Scripts/Jeff's Scripts/StoreLogic.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoreLogic : MonoBehaviour {
+    public GameObject StoreCan;
+    public GameObject Player;
+    public GameObject Store;
+    public GameObject TurretPrefab;
+    public GameObject Turret;
+    public float MaxDist;
+    public bool Creating;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.Distance(Player.transform.position, Store.transform.position) <= MaxDist)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                StoreCan.SetActive(true);
+            }
+        }
+        else
+        {
+            StoreCan.SetActive(false);
+        }
+
+        if (StoreCan.activeInHierarchy)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                StoreCan.SetActive(false);
+            }
+        }
+
+        if (Creating)
+        {
+            Turret.transform.position = (Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10)));
+            Turret.transform.position = new Vector3(Mathf.Clamp(Turret.transform.position.x, -6.59f, 6.59f), Mathf.Clamp(Turret.transform.position.y, -4.56f, 4.56f), Turret.transform.position.z);
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                Turret.transform.position = Turret.transform.position;
+                Creating = false;
+            }
+             
+        }
+	}
+
+    public void CreateTurret()
+    {
+        StoreCan.SetActive(false);
+        Creating = true;
+        Turret = Instantiate(TurretPrefab) as GameObject;
+    }
+}
